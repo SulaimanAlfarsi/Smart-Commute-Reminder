da6ac1733caf0050e4b3cb138f90ec69d22ce41f
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-29 11:06:03 +0400

    add http and json dependencies

--- src/main/java/com/smartcommute/reminder/SmartCommuteReminderApplication.java
+++ src/main/java/com/smartcommute/reminder/SmartCommuteReminderApplication.java
@@ -5,10 +5,5 @@ public final class SmartCommuteReminderApplication {
     }
 
     public static void main(String[] args) {
-        System.out.println(buildStartupMessage());
-    }
-
-    static String buildStartupMessage() {
-        return "Smart Commute Reminder is set up and ready.";
     }
 }
